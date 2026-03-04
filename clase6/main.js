af0e218ffcc833d536d9d12cc5a6c74481174940
nuriiii8671-droid <nuriiii8671@gmail.com>
2026-03-04 17:41:16 -0600

    subiendo clase 6 y clase 5

---
+++ clase6/main.js
@@ -0,0 +1,29 @@
+
+
+const lunes = "Lunes";
+const martes = "Martes";
+const miercoles = "Miercoles";
+const jueves = "Jueves";
+const viernes = "Viernes";
+const sabado = "Sabado";
+const domingo = "Domingo";
+
+const hoy = "Miercoles";
+
+const expresion1 = hoy == lunes;
+console.log("Hoy es lunes? " + expresion1);
+
+
+const expresion2 = hoy == miercoles;
+console.log("Hoy es miercoles?" + expresion2);
+
+const expresion3 = hoy == martes || hoy == miercoles;
+console.log("Hoy es martes o miercoles?" + expresion3);
+
+const expresion4 = hoy == miercoles && hoy == jueves;
+console.log("Hoy es miercoles y jueves?" + expresion4);
+
+
+
+
+
