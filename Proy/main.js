1e2123aa483f36fc97b9656b2bc5080788d0dd95
nuriiii8671-droid <nuriiii8671@gmail.com>
2026-04-28 13:03:21 -0600

    actualizando tarea

--- Proy/main.js
+++ Proy/main.js
@@ -5,22 +5,22 @@ let aprobados = [];
 let reprobados = [];
 let listaTransformada = [];
 
-// Recorremos la lista de 2 en 2 (i += 2)
+
 for (let i = 0; i < list.length; i += 2) {
     let nombre = list[i];
     let nota = list[i + 1];
     
-    // 1. Sumar para el promedio
+    
     sumaCalificaciones += nota;
 
-    // 2 y 3. Clasificar aprobados y reprobados (aprueban con 70)
+    
     if (nota >= 70) {
         aprobados.push(nombre);
     } else {
         reprobados.push(nombre);
     }
 
-    // 4. Convertir nota a letra
+    
     let letra = "";
     if (nota >= 95) { letra = "A"; }
     else if (nota >= 90) { letra = "B"; }
@@ -28,15 +28,15 @@ for (let i = 0; i < list.length; i += 2) {
     else if (nota >= 70) { letra = "D"; }
     else { letra = "F"; }
 
-    // Guardar en la nueva lista y mostrar log individual
+
     listaTransformada.push(nombre, letra);
     console.log(`-> La calificación de ${nombre} es ${letra}`);
 }
 
-// Cálculos finales
+
 let promedio = (sumaCalificaciones / (list.length / 2)).toFixed(2);
 
-// Outputs finales
+
 console.log("--- RESULTADOS ---");
 console.log(`-> El promedio de calificaciones es: ${promedio}`);
 console.log(`-> Los estudiantes que aprobaron son: ${aprobados.join(", ")}`);
